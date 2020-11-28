315b9de7edc792cb69e1101fb4b443a4ea26f38e
geeth22 <geethchandra22@gmail.com>
2020-11-29 02:53:51 +0530

    added:function to insert node at index

--- utility.js
+++ utility.js
@@ -31,5 +31,30 @@ class Linkedlist {
         }
         this.size++; 
     }
+    
+    insertAt= (data, index) => { 
+        if (index > 0 && index > this.size) {
+            return;
+        }
+        if (index === 0) {
+            this.insertFirst(data, this.head);
+            return;
+        }
+        const node = new Node(data);
+        let current, previous;
+
+        current = this.head;
+        let count = 0;
+
+        while(count < index){
+            previous = current; //node before index
+            count++;
+            current = current.next; // Node after index
+        }
+        node.next = current;
+        previous.next = node;
+
+        this.size++;
+    }
 }
 module.exports = new Linkedlist();
