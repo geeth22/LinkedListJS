cb00bb389aec21f5ee7e281a962c3a0ceecd0e2d
geeth22 <geethchandra22@gmail.com>
2020-11-29 02:36:26 +0530

    added:function to insert node at first

--- utility.js
+++ utility.js
@@ -1,2 +1,19 @@
-//Welcome to Linked list operations : Add new entries, delete etc..
+class Node {
+    constructor(data, next) {
+        this.data = data;
+        this.next = next;
+    }
+}
 
+class Linkedlist {
+    constructor() {
+        this.head = null;
+        this.size = 0;
+    }
+    /** Insert first node */
+    insertFirst(data) {
+        this.head = new Node(data, this.head);
+        return this;
+    }
+}
+module.exports = new Linkedlist();
