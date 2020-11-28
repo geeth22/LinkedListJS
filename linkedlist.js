92fb572be32ff2bd2f086c034774bd0aea287c8f
geeth22 <geethchandra22@gmail.com>
2020-11-29 02:35:05 +0530

    refactor:main file

--- linkedlist.js
+++ linkedlist.js
@@ -1,1 +1,5 @@
-//Welcome to Linkedlist operations
+const Utility = require('./Utility');
+let readlineSync = require('readline-sync');
+let data = readlineSync.question("Enter the value: ");
+let result = Utility.insertFirst(data);
+console.log(result);
