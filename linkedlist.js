d57aa3f4dc7e672e270cd1bfd6f0b6429d68a60b
geeth22 <geethchandra22@gmail.com>
2020-11-29 02:45:57 +0530

    refactor:check option to insert node

--- linkedlist.js
+++ linkedlist.js
@@ -1,5 +1,23 @@
 const Utility = require('./Utility');
 let readlineSync = require('readline-sync');
-let data = readlineSync.question("Enter the value: ");
-let result = Utility.insertFirst(data);
-console.log(result);
+checkoption = function () {
+    let isTerminated = true;
+    while (isTerminated == true) {
+        let checkoption = readlineSync.question("\nEner your choice:\n1.firstNode\n2.lastNode\n3.exit\n");
+        switch (checkoption) {
+            case '1':
+                let firstPosition = readlineSync.question("Enter the element in first position: ");
+                Utility.insertFirst(firstPosition);
+                break;
+            case '2':
+                let lastPosition = readlineSync.question("Enter the element in last position: ");
+                Utility.insertLast(lastPosition);
+                break;
+            case '3':
+                isTerminated = false;
+                break;
+            default: console.log("please enter correct option");
+        }
+    }
+}
+checkoption();
